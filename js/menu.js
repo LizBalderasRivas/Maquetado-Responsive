e88c11fc3b252f54acdb1bc05c03f71de45e9a19
Lizbeth Balderas <lizbalderasrivas@gmail.com>
2021-03-23 21:27:18 -0600

    Maquetado Responsivo

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+ //MENU//
+ $(document).ready(function(){
+    $('.btn').click(function(){
+      $('.items').toggleClass("show");
+      $('ul li').toggleClass("hide");
+    });
+  });
+
+
+//SLIDER//
+
+$(document).ready(function(){
+   $('.banners').slick({
+      
+    });
+ });
+
+
+ 
+  
+  
+
